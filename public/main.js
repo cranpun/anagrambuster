06641085fd1b02cd0b0fd749b7e5a190d51b729a
cranpun <cranpun@gmail.com>
2019-03-25 19:14:04 +0900

    バックエンド除外

--- public/main.js
+++ public/main.js
@@ -14,23 +14,74 @@ class AnagramBusterMain extends React.Component {
 
         // This binding is necessary to make `this` work in the callback
         this.buster = this.buster.bind(this);
+        this.makeAnagrams = this.makeAnagrams.bind(this);
         this.makeTwButton = this.makeTwButton.bind(this);
     }
+
+
+    makeAnagrams(head, arr, shuffle) {
+
+        try {
+            const skipCap = [
+                "っ", "ん", "ゃ", "ゅ", "ょ", "ぁ", "ぃ", "ぅ", "ぇ", "ぉ",
+            ];
+            if (head.length >= shuffle - 1) {
+                // ここでさらに一文字足すのでshuffleは-1
+                const ret = [];
+                for (let i = 0; i < arr.length; i++) {
+                    const remines = arr.concat(); // 複製
+                    const char = remines.splice(i, 1)[0];
+                    const anagram = head + char;
+                    const data = {
+                        anagram: anagram,
+                        remines: remines,
+                    };
+                    // console.log(data);
+                    ret.push(data);
+                }
+                return ret;
+            } else {
+                // 次へ
+                let ret = [];
+                for (let i = 0; i < arr.length; i++) {
+                    // 配列をコピー
+                    const next = arr.concat(); // 複製
+                    const char = next.splice(i, 1)[0];
+                    const text = head + char;
+                    if (head.length === 0) {
+                        // 先頭にはつかない文字なら飛ばす
+                        if (skipCap.includes(char)) {
+                            continue;
+                        }
+                    }
+                    const ana = this.makeAnagrams(text, next, shuffle);
+                    ret = ret.concat(ana);
+                }
+                return ret;
+            }
+        } catch (e) {
+            // エラー
+            return [];
+        }
+    };
 
     buster() {
         this.setState({ inst: false });
         setTimeout(() => {
             // パラメータの取得
             const eText = document.querySelector("#text");
-            const vText = encodeURI(eText.value);
             const eShuffle = document.querySelector("#shuffle");
-            const vShuffle = encodeURI(eShuffle.value);
 
-            ky.default(`/api/buster?text=${vText}&shuffle=${vShuffle}`).json().then((res) => {
-                this.setState(res);
-                this.makeTwButton();
-                this.setState({ inst: true });
-            });
+            const arr = eText.split('');
+            if (arr.length === 0) {
+                // 文字がないため終了
+                return [];
+            }
+            const ret = this.makeAnagrams("", arr, eShuffle);
+
+            this.setState(ret);
+            this.makeTwButton();
+            this.setState({ inst: true });
         }, 500);
     }
 
